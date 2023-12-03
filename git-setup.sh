6c89d5247479208c9bfdc194919579c431171fed
halt23 <maassam495@gmail.com>
2023-12-03 17:17:10 +0100

    first commit

---
+++ git-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git remote set-url origin git@sammaas.com:/var/www/git/satisfactory.git
+git remote add github git@github.com:halt23/satisfactory.git
---
+++ push.sh
@@ -0,0 +1,30 @@
+echo "##########################################################"
+echo "This script wil push to the main git server and to github!"
+echo "##########################################################"
+echo "Checking for newer files online first"
+git pull
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "#######################################"
+echo "Write your commit comment if aplicable!"
+echo "######################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+
+
+git push -u origin master
+git push -u github master
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
